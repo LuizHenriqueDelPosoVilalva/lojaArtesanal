adcb30ce4c3f7240d5dfe241d598db328ca4f5dc
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2025-05-07 21:27:08 -0400

    enviado servidor para repositório server

--- routes/autenticacaoRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var { login, logout, autenticar } = require('../controllers/autenticacaoController');
-
-router.post('/login', login);
-router.get('/logout', logout);
-router.get('/', autenticar);
-
-module.exports = router
