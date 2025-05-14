e37badc37b8d76ab1d17991193aa330f70beb8c9
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2025-05-13 22:06:16 -0400

    adicionado client

---
+++ client/src/components/h1.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps({
+        titulo: string
+    })
+</script>
+
+<template>
+    <h1><slot>{{ titulo }}</slot></h1>
+</template>
+
+<style scoped>
+    
+</style>
---
+++ client/src/views/SobreView.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import H1 from '@/components/h1.vue'
+</script>
+
+<template>
+    <H1 titulo="Eu sou um h1"/>
+    <H1>Teste</H1>
+    <H1>AAAAAAAAAA</H1>
+</template>
+
+<style>
+
+</style>
