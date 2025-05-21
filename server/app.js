269b3ac23a8b685dae4cc958bcb5b389da4cddb4
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2025-05-21 18:16:53 -0400

    comentado views na raiz do server

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ var app = express();
 
 mongooseConfig
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -55,7 +55,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   console.log(err)
-  res.render('error', {mensagem: "Algo deu errado"});
+  //res.render('error', {mensagem: "Algo deu errado"});
 });
 
 module.exports = app;
